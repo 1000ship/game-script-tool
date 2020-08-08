2dd155ab8fc548f7c565b97ab7d7aa06671a565e
1000ship <cjstjdgur1234@gmail.com>
2020-08-08 15:09:49 +0900

    Solve some warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ControlMenu from "./Components/ControlMenu";
 import ScriptView from "./Components/ScriptView";
--- src/Components/ScriptView.js
+++ src/Components/ScriptView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import ScriptBlock from "./Partials/ScriptBlock";
 import { saveBlockList } from "../Utils/api";
